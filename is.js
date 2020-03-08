325453ab38489d79eb86c642e2f6a4ec34228810
Adam Nathaniel Davis <adam.davis@hashverify.com>
2020-03-08 13:50:26 -0400

    Update is.js

--- is.js
+++ is.js
@@ -1,76 +1,83 @@
 class booleanValidation {
-   aBoolean(value) {
-      return (typeof value === 'boolean');
-   }
+   aBoolean = (value = false) => {
+      if (typeof value === 'boolean')
+         return true;
+      console.error(value, 'is not a Boolean');
+      return false;
+   };
    
-   aGuid(value) {
-      if (!this.aPopulatedString(value)) return false;
-      if (value.length !== 32) { return false; }
-      for (let i = 0; i < 32; i++) {
-         const characterCode = value.charCodeAt(i);
-         if (i === 0) {
-            if (!(characterCode >= 65 && characterCode <= 90)) { return false; }
-         } else {
-            if (!(characterCode >= 48 && characterCode <= 57) && !(characterCode >= 65 && characterCode <= 90)) { return false; }
-         }
+   anArray = (value = []) => {
+      if (Array.isArray(value))
+         return true;
+      console.error(value, 'is not an array');
+      return false;
+   };
+   
+   anInteger = (value = 0) => {
+      if (Number.isInteger(value))
+         return true;
+      console.error(value, 'is not an integer');
+      return false;
+   };
+   
+   anObject = (value = {}) => {
+      if (typeof value === 'object' && !Array.isArray(value) && value !== null)
+         return true;
+      console.error(value, 'is not an object');
+   };
+   
+   aNonNegativeInteger = (value = {}) => {
+      return this.anInteger(value) && value >= 0;
+   };
+   
+   aNumber = (value = 0) => {
+      if (typeof value === 'number')
+         return true;
+      console.error(value, 'is not a number');
+      return false;
+   };
+   
+   aPopulatedArray = (value = []) => {
+      if (Array.isArray(value) && value.length > 0)
+         return true;
+      console.error(value, 'is not a populated array');
+      return false;
+   };
+   
+   aPopulatedObject = (value = {}) => {
+      if (this.nullOrUndefined(value)) {
+         console.error(value, 'is not a populated object');
+         return false;
       }
-      return true;
-   }
+      if (typeof value === 'object' && !Array.isArray(value) && Object.keys(value).length > 0)
+         return true;
+      console.error(value, 'is not a populated object');
+      return false;
+   };
    
-   anArray(value) {
-      return Array.isArray(value);
-   }
+   aPopulatedString = (value = '') => {
+      if (typeof value === 'string' && value.trim() !== '')
+         return true;
+      console.error(value, 'is not a populated string');
+      return false;
+   };
    
-   anInteger(value) {
-      return Number.isInteger(value);
-   }
+   aPositiveInteger = (value = {}) => {
+      return this.anInteger(value) && value > 0;
+   };
    
-   anObject(value) {
-      return (typeof value === 'object' && !Array.isArray(value) && value !== null);
-   }
+   aString = (value = '') => {
+      if (typeof value === 'string')
+         return true;
+      console.error(value, 'is not a string');
+      return false;
+   };
    
-   aNonnegativeInteger(value) {
-      return Number.isInteger(value) && value >= 0;
-   }
+   null = variable => variable === null;
    
-   aNumber(value) {
-      return typeof value === 'number';
-   }
+   nullOrUndefined = (variable) => (variable === undefined || variable === null);
    
-   aPopulatedArray(value) {
-      if (!Array.isArray(value)) return false;
-      return !!value.length;
-   }
-   
-   aPopulatedObject(value) {
-      if (this.nullOrUndefined(value)) return false;
-      return (typeof value === 'object' && !Array.isArray(value) && Object.keys(value).length > 0);
-   }
-   
-   aPopulatedString(value) {
-      return (typeof value === 'string' && value.trim() !== '');
-   }
-   
-   aString(value) {
-      return (typeof value === 'string');
-   }
-   
-   null(variable) {
-      return variable === null;
-   }
-   
-   nullOrUndefined(variable) {
-      return (variable === undefined || variable === null);
-   }
-   
-   oneOfStrings(variable, oneOf = []) {
-      if (!this.aPopulatedString(variable) || !this.aPopulatedArray(oneOf)) return false;
-      return oneOf.some(acceptableValue => acceptableValue === variable);
-   }
-   
-   undefined(variable) {
-      return variable === undefined;
-   }
+   undefined = variable => variable === undefined;
 }
 
 const is = new booleanValidation();
