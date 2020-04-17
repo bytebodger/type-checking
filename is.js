145b4ec3ce2f604c212f941e6d1767a614a385d4
Adam Nathaniel Davis <adam.davis@hashverify.com>
2020-04-17 16:13:43 -0400

    Update is.js

--- is.js
+++ is.js
@@ -1,84 +1,172 @@
-class booleanValidation {
-   aBoolean = (value = false) => {
+export default class is {
+   /**
+    * @param {boolean} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static aBoolean = (value = false, suppressError = false) => {
       if (typeof value === 'boolean')
          return true;
-      console.error(value, 'is not a Boolean');
+      if (!suppressError)
+         console.error(value, 'is not a Boolean');
       return false;
    };
    
-   anArray = (value = []) => {
+   /**
+    * @param {function} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static aFunction = (value = () => {}, suppressError = false) => {
+      if (typeof value === 'function')
+         return true;
+      if (!suppressError)
+         console.error(value, 'is not a function');
+      return false;
+   };
+   
+   /**
+    * @param {[]} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static anArray = (value = [], suppressError = false) => {
       if (Array.isArray(value))
          return true;
-      console.error(value, 'is not an array');
+      if (!suppressError)
+         console.error(value, 'is not an array');
       return false;
    };
    
-   anInteger = (value = 0) => {
+   /**
+    * @param {number} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static anInteger = (value = 0, suppressError = false) => {
       if (Number.isInteger(value))
          return true;
-      console.error(value, 'is not an integer');
+      if (!suppressError)
+         console.error(value, 'is not an integer');
       return false;
    };
    
-   anObject = (value = {}) => {
+   /**
+    * @param {Object} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static anObject = (value = {}, suppressError = false) => {
       if (typeof value === 'object' && !Array.isArray(value) && value !== null)
          return true;
-      console.error(value, 'is not an object');
+      if (!suppressError)
+         console.error(value, 'is not an object');
+      return false;
    };
    
-   aNonNegativeInteger = (value = -1) => {
-      return this.anInteger(value) && value >= 0;
+   /**
+    * @param {number} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static aNonNegativeInteger = (value = -1, suppressError = false) => {
+      return this.anInteger(value, suppressError) && value >= 0;
    };
    
-   aNumber = (value = 0) => {
+   /**
+    * @param {number} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static aNumber = (value = 0, suppressError = false) => {
       if (typeof value === 'number')
          return true;
-      console.error(value, 'is not a number');
+      if (!suppressError)
+         console.error(value, 'is not a number');
       return false;
    };
    
-   aPopulatedArray = (value = []) => {
+   /**
+    * @param {[]} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static aPopulatedArray = (value = [], suppressError = false) => {
       if (Array.isArray(value) && value.length > 0)
          return true;
-      console.error(value, 'is not a populated array');
+      if (!suppressError)
+         console.error(value, 'is not a populated array');
       return false;
    };
    
-   aPopulatedObject = (value = {}) => {
+   /**
+    * @param {Object} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static aPopulatedObject = (value = {}, suppressError = false) => {
       if (this.nullOrUndefined(value)) {
-         console.error(value, 'is not a populated object');
+         if (!suppressError)
+            console.error(value, 'is not a populated object');
          return false;
       }
       if (typeof value === 'object' && !Array.isArray(value) && Object.keys(value).length > 0)
          return true;
-      console.error(value, 'is not a populated object');
+      if (!suppressError)
+         console.error(value, 'is not a populated object');
       return false;
    };
    
-   aPopulatedString = (value = '') => {
+   /**
+    * @param {string} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static aPopulatedString = (value = '', suppressError = false) => {
       if (typeof value === 'string' && value.trim() !== '')
          return true;
-      console.error(value, 'is not a populated string');
+      if (!suppressError)
+         console.error(value, 'is not a populated string');
       return false;
    };
    
-   aPositiveInteger = (value = -1) => {
-      return this.anInteger(value) && value > 0;
+   /**
+    * @param {number} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static aPositiveInteger = (value = -1, suppressError = false) => {
+      return this.anInteger(value, suppressError) && value > 0;
    };
    
-   aString = (value = '') => {
+   /**
+    * @param {string} value
+    * @param {boolean} suppressError
+    * @returns {boolean}
+    */
+   static aString = (value = '', suppressError = false) => {
       if (typeof value === 'string')
          return true;
-      console.error(value, 'is not a string');
+      if (!suppressError)
+         console.error(value, 'is not a string');
       return false;
    };
    
-   null = variable => variable === null;
+   /**
+    * @param {*} variable
+    * @returns {boolean}
+    */
+   static null = variable => variable === null;
    
-   nullOrUndefined = (variable) => (variable === undefined || variable === null);
+   /**
+    * @param {*} variable
+    * @returns {boolean}
+    */
+   static nullOrUndefined = variable => (variable === undefined || variable === null);
    
-   undefined = variable => variable === undefined;
+   /**
+    * @param {*} variable
+    * @returns {boolean}
+    */
+   static undefined = variable => variable === undefined;
 }
-
-const is = new booleanValidation();
-export default is;
